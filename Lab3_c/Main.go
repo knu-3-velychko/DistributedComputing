cd2957fd4896e55b4766c72147a78986a6b842e8
Tai <justataya@gmail.com>
2020-02-08 20:54:42 +0300

    Added Lab3_c

--- Lab3_c/Main.go
+++ Lab3_c/Main.go
@@ -3,22 +3,36 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"sync"
 	"time"
 )
 
 //component can be tabacco==0, paper==1,matches==2
-func smoker(component int, table *[]bool, readSemaphore, writeSemaphore chan bool) {
+func smoker(component int, table *[]bool, readSemaphore, writeSemaphore chan bool, waitGroup *sync.WaitGroup) {
+	defer waitGroup.Done()
 	for {
 		readSemaphore <- true
 		fmt.Println("Checking table # ", component, "...")
+		if !(*table)[component] {
+			fmt.Println("Smoking # ", component, "...")
+			for i := range *table {
+				fmt.Println((*table)[i])
+				(*table)[i] = false
+			}
+			writeSemaphore <- true
+		} else {
+			<-readSemaphore
+			time.Sleep(time.Second)
+		}
 	}
 }
 
-func intermediary(table *[]bool, readSemaphore, writeSemaphore chan bool) {
+func intermediary(table *[]bool, readSemaphore, writeSemaphore chan bool, waitGroup *sync.WaitGroup) {
+	defer waitGroup.Done()
 	for {
 		<-writeSemaphore
-		fmt.Println("Intermediary put new items:")
 		var first, second = getCigaretteStuff()
+		fmt.Println("Intermediary put new items:", first, "and", second)
 		(*table)[first] = true
 		(*table)[second] = true
 		<-readSemaphore
@@ -39,15 +53,19 @@ func getCigaretteStuff() (int, int) {
 
 func main() {
 	const smockersNum = 3
+	var waitGroup sync.WaitGroup
 	var table = make([]bool, smockersNum)
 
 	var readSemaphore = make(chan bool)
-	var writeSemaphore = make(chan bool)
+	var writeSemaphore = make(chan bool, 1)
 
 	writeSemaphore <- true
-	go intermediary(&table, readSemaphore, writeSemaphore)
+	waitGroup.Add(1)
+	go intermediary(&table, readSemaphore, writeSemaphore, &waitGroup)
 
 	for i := 0; i < smockersNum; i++ {
-		go smoker(i, &table, readSemaphore, writeSemaphore)
+		waitGroup.Add(1)
+		go smoker(i, &table, readSemaphore, writeSemaphore, &waitGroup)
 	}
+	waitGroup.Wait()
 }
