92aa84731c249204de1bc6a72a60208eb0454b24
Taya <justataya@gmail.com>
2020-03-25 11:44:07 -0700

    Progress commit

--- Lab9_2/Main.go
+++ Lab9_2/Main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"math/rand"
+	"os"
+	"strconv"
 	"sync"
 	"time"
 )
@@ -77,6 +80,115 @@ func calculateEntry(A, B [][]float32, i, j int) float32 {
 	}
 
 	return result
+}
+
+func createHTML(name string) *os.File {
+	f, err := os.Create(name + ".html")
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+
+	return f
+}
+
+func addHead(f *os.File) {
+	_, err := f.WriteString(`<html xmlns="http://www.w3.org/1999/xhtml">
+	<head><title>Reporting Page</title>
+		<link rel="stylesheet" href="style.css"></link>
+	</head>`)
+
+	if err != nil {
+		fmt.Println(err)
+		f.Close()
+		return
+	}
+}
+
+func startBody(f *os.File) {
+	_, err := f.WriteString("<body>")
+
+	if err != nil {
+		fmt.Println(err)
+		f.Close()
+		return
+	}
+}
+
+func finishBody(f *os.File) {
+	_, err := f.WriteString("</body>")
+
+	if err != nil {
+		fmt.Println(err)
+		f.Close()
+		return
+	}
+}
+
+func createTable(f *os.File) {
+	_, err := f.WriteString(`<table>
+    <tr>
+        <th rowspan=\"3\">Matrix size</th>
+        <th rowspan=\"3\">Sequential algorithm</th>
+        <th colspan=\"4\">Parallel algorithm</th>
+    </tr>
+    <tr>
+        <th colspan=\"2\">2 processes</th>
+        <th colspan=\"2\">4 processes</th>
+    </tr>
+    <tr>
+        <th>Time</th>
+        <th>Acceleration</th>
+        <th>Time</th>
+        <th>Acceleration</th>
+	</tr>`)
+
+	if err != nil {
+		fmt.Println(err)
+		f.Close()
+		return
+	}
+}
+
+//FloatPair : type that represents pair class for float32
+type FloatPair struct {
+	first, second float32
+}
+
+func addResult(f *os.File, matrixSize int, sequenialTime float32, results []FloatPair) {
+	resultString := "<tr>"
+	resultString += "<td>" + strconv.Itoa(matrixSize) + "/<td>"
+	resultString += "<td>" + fmt.Sprintf("%f", sequenialTime) + "/<td>"
+
+	for i := range results {
+		resultString += "<td>" + fmt.Sprintf("%f", results[i].first) + "</td>"
+		resultString += "<td>" + fmt.Sprintf("%f", results[i].second) + "</td>"
+	}
+	resultString += "</tr>"
+
+	_, err := f.WriteString(resultString)
+
+	if err != nil {
+		fmt.Println(err)
+		f.Close()
+		return
+	}
+}
+
+func closeHTML(f *os.File) {
+	_, err := f.WriteString("</html>")
+
+	if err != nil {
+		fmt.Println(err)
+		f.Close()
+		return
+	}
+
+	err = f.Close()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 }
 
 func test() {
