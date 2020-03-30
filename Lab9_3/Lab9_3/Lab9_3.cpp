511af12fadbe8926f749d5c9241cb738b75882b2
Taya <justataya@gmail.com>
2020-03-30 11:12:58 -0700

    Progress commit

--- Lab9_3/Lab9_3/Lab9_3.cpp
+++ Lab9_3/Lab9_3/Lab9_3.cpp
@@ -1,10 +1,19 @@
 // Lab9_3.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+#include "Test.h"
 #include <iostream>
 #include <tbb/parallel_for.h>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	auto test = new Test("/resoutce/index.html");
+	test->addTask(100);
+	test->addTask(500);
+	test->addTask(1000);
+	test->addTask(1500);
+	test->addTask(2000);
+	test->addTask(2500);
+	test->addTask(3000);
+	test->run();
 }
